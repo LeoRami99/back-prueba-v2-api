a6a7ad1831343dcc72f3d4f383524f73b2f9ef5e
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-18 17:58:45 -0500

    Add GetCardToken use case for retrieving card tokens

---
+++ src/modules/card/application/use-cases/get-token-card.use-case.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CardEntity } from '../../domain/entities/card.entity';
+import { CardTokenEntity } from '../../domain/entities/card.token.entity';
+import { CardRepository } from '../../domain/repositories/card.repository';
+
+@Injectable()
+export class GetCardTokenUseCase {
+  constructor(private readonly cardRepository: CardRepository) {}
+
+  async execute(data: CardEntity): Promise<CardTokenEntity> {
+    const cardToken: CardTokenEntity =
+      await this.cardRepository.getCardToken(data);
+    if (!cardToken) {
+      throw new Error('Error creating card token');
+    }
+    return cardToken;
+  }
+}
