61a0e5643fa832cdcc0cd3656d9995940bdd6162
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-18 18:19:38 -0500

    Refactor create method to improve error handling with specific exceptions

--- src/modules/transaction/infrastructure/http/transaction.externals.repository.impl.ts
+++ src/modules/transaction/infrastructure/http/transaction.externals.repository.impl.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { TransactionExternalRepository } from '../../domain/repositories/transaction.repository';
 import { HttpService } from '@nestjs/axios';
 import { TransactionExternal } from '../../domain/entities/transaction.external.entity';
@@ -13,26 +17,36 @@ export class TransactionExternalRepositoryImpl
   async create(
     transactionData: Partial<TransactionExternal>,
   ): Promise<ApiPaymentResult> {
-    const url = process.env.UAT_SANDBOX_URL as string;
-    if (!url) {
-      throw new Error('UAT_SANDBOX_URL is not defined');
-    }
-    const uri = `${url}/transactions`;
-    if (!transactionData) {
-      throw new Error('Transaction data is required');
-    }
-    const response = await firstValueFrom(
-      this.httpService.post<ApiPaymentResult>(uri, transactionData, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.PRIVATE_KEY as string}`,
-        },
-      }),
-    );
-    if (!response?.data) {
-      throw new Error('Error creating transaction');
-    }
+    try {
+      const url = process.env.UAT_SANDBOX_URL as string;
+      if (!url) {
+        throw new UnprocessableEntityException(
+          'UAT_SANDBOX_URL is not defined',
+        );
+      }
+      const uri = `${url}/transactions`;
+      if (!transactionData) {
+        throw new UnprocessableEntityException('Transaction data is required');
+      }
+      const response = await firstValueFrom(
+        this.httpService.post<ApiPaymentResult>(uri, transactionData, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.PRIVATE_KEY as string}`,
+          },
+        }),
+      );
+      if (!response?.data) {
+        throw new InternalServerErrorException('Error creating transaction');
+      }
 
-    return response.data as ApiPaymentResult;
+      return response.data;
+    } catch (error: unknown) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      throw new InternalServerErrorException(
+        `Error creating external transaction - ${errorMessage}`,
+      );
+    }
   }
 }
