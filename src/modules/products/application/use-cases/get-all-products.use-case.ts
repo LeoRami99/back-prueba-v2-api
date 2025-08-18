4b58b925833975949ca70025e80ebeb2925bead7
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-18 17:59:20 -0500

    Add GetAllProductsUseCase to retrieve paginated product listings with optional filtering

---
+++ src/modules/products/application/use-cases/get-all-products.use-case.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsRepository } from '../../domain/repositories/products.repository';
+
+import { ProductsEntity } from '../../domain/entities/products.entity';
+
+@Injectable()
+export class GetAllProductsUseCase {
+  constructor(private readonly productsRepository: ProductsRepository) {}
+  async execute(
+    page: number = 1,
+    pageSize: number = 5,
+    filter?: string,
+  ): Promise<{
+    products: ProductsEntity[];
+    total: number;
+    pages: number;
+  }> {
+    return this.productsRepository.getAll(page, pageSize, filter);
+  }
+}
