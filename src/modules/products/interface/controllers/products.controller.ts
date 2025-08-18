0860eb84c4d43083d14e1ba0a4a4c03e6eff3b06
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-18 17:59:41 -0500

    Add ProductsController to handle product retrieval requests

---
+++ src/modules/products/interface/controllers/products.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Body,
+  Get,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { GetAllProductsUseCase } from '../../application/use-cases/get-all-products.use-case';
+import { GetProductsDto } from '../dtos/get-products.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly getAllProducta: GetAllProductsUseCase) {}
+
+  @Get()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async finAll(@Query() query: GetProductsDto) {
+    const { page, pageSize, filter } = query;
+    return this.getAllProducta.execute(page, pageSize, filter);
+  }
+}
