d9fba165ad3037e1261d7b1f84c5fb9309eb16ed
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-18 18:00:09 -0500

    Add TransactionEntity class to define transaction structure and properties

---
+++ src/modules/transaction/domain/entities/transaction.entity.ts
@@ -0,0 +1,13 @@
+export class TransactionEntity {
+  constructor(
+    public amount: number,
+    public userId: string,
+    public methodPayment: string,
+    public productId: string,
+    public price: number,
+    public referenceInternalTransaction?: string,
+    public idExternalTransaction?: string,
+    public id?: string,
+    public status?: string,
+  ) {}
+}
