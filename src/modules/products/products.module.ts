5454b58434d200555a2251e3c3fdfe87df131fc4
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-19 01:01:44 -0500

    Refactor ProductsModule to include UpdateStockUseCase and correct repository import path

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -5,17 +5,20 @@ import { ProductModel } from './infrastructure/database/product.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { GetAllProductsUseCase } from './application/use-cases/get-all-products.use-case';
 import { ProductsRepository } from './domain/repositories/products.repository';
-import { ProductsRepositoryImpl } from './infrastructure/database/products.repository';
+import { ProductsRepositoryImpl } from './infrastructure/database/products.repository.impl';
+import { UpdateStockUseCase } from './application/use-cases/update-stock.use-case';
 
 @Module({
   imports: [SequelizeModule.forFeature([ProductModel])],
   controllers: [ProductsController],
   providers: [
     GetAllProductsUseCase,
+    UpdateStockUseCase,
     {
       provide: ProductsRepository,
       useClass: ProductsRepositoryImpl,
     },
   ],
+  exports: [UpdateStockUseCase, ProductsRepository],
 })
 export class ProductsModule {}
