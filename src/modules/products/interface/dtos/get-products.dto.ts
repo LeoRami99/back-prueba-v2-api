bc3263ce09c08e9a3d95783a70596792b9009592
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-19 01:25:41 -0500

    Enhance GetProductsDto validation by adding Min and Max constraints for pagination

--- src/modules/products/interface/dtos/get-products.dto.ts
+++ src/modules/products/interface/dtos/get-products.dto.ts
@@ -1,5 +1,12 @@
 // src/products/dto/get-products.dto.ts
-import { IsInt, IsOptional, IsPositive, IsString } from 'class-validator';
+import {
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class GetProductsDto {
@@ -7,12 +14,16 @@ export class GetProductsDto {
   @IsPositive()
   @Type(() => Number)
   @IsOptional()
+  @Min(1)
+  @Type(() => Number)
   page: number;
 
   @IsInt()
   @IsPositive()
   @Type(() => Number)
   @IsOptional()
+  @Min(1)
+  @Max(100)
   pageSize: number;
 
   @IsOptional()
