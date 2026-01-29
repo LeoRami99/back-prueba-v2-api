7a1dc75bdea2f638ef4227a0c14e6bb28df54d4c
Leonardo Ramírez <juanlov4321@gmail.com>
2026-01-28 22:21:20 -0500

    Refactor TransactionController to correctly implement getTransactionByInternalId method

--- src/modules/transaction/interface/controllers/transaction.controller.ts
+++ src/modules/transaction/interface/controllers/transaction.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { CreateTransactionUseCase } from '../../application/use-cases/create-transaction.use-case';
 import { GetTransactionByIdUseCase } from '../../application/use-cases/get-transaction-id.use-case';
+import { GetTransactionByInternalIdUseCase } from '../../application/use-cases/get-transaction-internal-id.use-case';
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
 
 @Controller('transactions')
@@ -8,6 +9,7 @@ export class TransactionController {
   constructor(
     private readonly createUseCase: CreateTransactionUseCase,
     private readonly getTransactionByIdUseCase: GetTransactionByIdUseCase,
+    private readonly getTransactionByInternalIdUseCase: GetTransactionByInternalIdUseCase,
   ) {}
 
   @Post()
@@ -30,12 +32,12 @@ export class TransactionController {
       installments,
     );
   }
+  @Get('internal/:idInternal')
+  async getTransactionByInternalId(@Param('idInternal') idInternal: string) {
+    return this.getTransactionByInternalIdUseCase.execute(idInternal);
+  }
   @Get(':id')
   async getTransactionById(@Param('id') id: string) {
     return this.getTransactionByIdUseCase.execute(id);
-  }
-  @Get('internal/:idInternal')
-  async getTransactionByInternalId(@Body('idInternal') idInternal: string) {
-    return this.getTransactionByIdUseCase.execute(idInternal);
   }
 }
