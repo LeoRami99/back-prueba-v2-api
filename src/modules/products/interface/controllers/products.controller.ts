5fffcf59a09cc82e5c19c851435331f382651fe4
Leonardo Ramírez <juanlov4321@gmail.com>
2026-01-28 22:57:51 -0500

    Enhance ProductsController with Swagger documentation for API operations

--- src/modules/products/interface/controllers/products.controller.ts
+++ src/modules/products/interface/controllers/products.controller.ts
@@ -8,12 +8,16 @@ import {
 } from '@nestjs/common';
 import { GetAllProductsUseCase } from '../../application/use-cases/get-all-products.use-case';
 import { GetProductsDto } from '../dtos/get-products.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly getAllProducta: GetAllProductsUseCase) {}
 
   @Get()
+  @ApiOperation({ summary: 'List products with pagination' })
+  @ApiResponse({ status: 200, description: 'Products list' })
   @UsePipes(new ValidationPipe({ transform: true }))
   async finAll(@Query() query: GetProductsDto) {
     const { page = 1, pageSize = 5, filter = undefined } = query;
